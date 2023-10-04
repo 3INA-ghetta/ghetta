2cb2409e8ac53d6490362c917e6b3a9fe8ee4b3c
claudio.ghetta <claudio.ghetta@AULAS51-PC24.itib.local>
2023-10-04 10:04:04 +0200

    modificato proprietà progetto

--- src/figureGeometriche/RettangoloTest.java
+++ src/figureGeometriche/RettangoloTest.java
@@ -14,17 +14,17 @@ public class RettangoloTest {
         float base;
         float altezza;
         
-        System.out.println("inserire il valore della base: ");
+        System.out.println("inserire il valore della base   : ");
         base = in.nextFloat();
         
-        System.out.println("inserire il valore dell altezza: ");
+        System.out.println("inserire il valore dell altezza : ");
         altezza = in.nextFloat();
         
         Rettangolo rettangolo = new Rettangolo (base,altezza);
         
-        base = rettangolo.b;
-        altezza = rettangolo.h;
-        area = rettangolo.calcolaArea();
+        base      = rettangolo.b;
+        altezza   = rettangolo.h;
+        area      = rettangolo.calcolaArea();
         perimetro = rettangolo.calcolaPerimetro();
         
         System.out.println("base       : " + base);
