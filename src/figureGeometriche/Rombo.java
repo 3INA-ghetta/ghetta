9f2823637587f83105df7923199e589590f1a371
claudio <claudio@192.168.10.111>
2023-10-17 21:35:49 +0200

    fatta classe Rombo

--- src/figureGeometriche/Rombo.java
+++ src/figureGeometriche/Rombo.java
@@ -3,8 +3,8 @@ package figureGeometriche;
 
 
 public class Rombo {
-    public float lato;
-    public float altezza;
+    private float lato;
+    private float altezza;
     
     public Rombo (float lato, float altezza) {
         this.lato    = lato;
@@ -12,9 +12,38 @@ public class Rombo {
         
     }
     
+    public double getLato(){
+        return lato;
+    }
+    
+    public double getAltezza(){
+        return altezza;
+    }
+    
+    public void setLato (float lato){
+        this.lato = lato; 
+    }
+    
+    public void setAltezza (double base){
+        this.altezza = altezza; 
+    }
+    
     public float calcolaArea () {
         float area;
         area = lato*altezza;
         return area;
+    }
+    
+    public float calcolaPerimetro () {
+        float perimetro;
+        perimetro = lato*4;
+        return perimetro;
+    }
+    
+     public String info(){
+        String testo;
+        testo = "altezza: " + altezza + "\n" +
+                "lato   : " + lato    + "\n";
+        return testo;
     }
 }
--- src/figureGeometriche/RomboTest.java
+++ src/figureGeometriche/RomboTest.java
@@ -1,12 +1,32 @@
 
 package figureGeometriche;
 
+import java.util.Scanner;
 
 public class RomboTest {
 
 
     public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        float l, a;
+        
+        System.out.println("Altezza:");
+        a = in.nextFloat();
+        
+        System.out.println("Lato   :");
+        l = in.nextFloat();
+        
+        Rombo r = new Rombo (l,a);
+        
+        System.out.println("Dati in input:");
+        System.out.println(r.info());
+        
+        System.out.println("Dati di output:");
+        System.out.println("Perimetro     : " + r.calcolaArea());
+        System.out.println("Area          : " + r.calcolaPerimetro());
 
     }
     
 }
+
