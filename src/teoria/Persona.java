b64ea4f846abd55d242f6884f9452d7f444b7e1f
claudio.ghetta <claudio.ghetta@AULAS51-PC22.itib.local>
2023-11-08 09:09:03 +0100

    esercitazione persona

---
+++ src/teoria/Persona.java
@@ -0,0 +1,53 @@
+
+package teoria;
+
+/**
+ *
+ * @author claudio.ghetta
+ */
+public class Persona {
+    private String nome;
+    private String cognome;
+    private String email;
+
+    
+    public Persona () {
+        
+    }
+    
+    public Persona (String nome ,String cognome, String email) {
+       this.nome=nome;
+       this.cognome=cognome;
+       this.email=email;           
+    }
+    
+    public String getNome () {
+        return nome;
+    }
+    public String getCognome () {
+        return cognome;
+    }
+    public String getEmail () {
+        return email;
+    }
+    
+    public boolean registrazioneAvvenuta () {
+        boolean registrazione;
+        if (email.equals(" ")) {
+        registrazione = false;
+        } else {
+        registrazione = true;
+        }
+        return registrazione;
+    }
+    
+    public String info () {
+        String testo;
+        testo = "nome          : " + nome    + "\n" +
+                "cognome       : " + cognome + "\n" +
+                "email         : " + email   + "\n";
+        return testo;
+        
+    }
+    
+}
---
+++ src/teoria/PersonaTest.java
@@ -0,0 +1,37 @@
+
+package teoria;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author claudio.ghetta
+ */
+public class PersonaTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        String n, c, e;
+               
+        System.out.println("nome   :");
+        n = in.nextLine();
+        
+        System.out.println("cognome   :");
+        c = in.nextLine();
+        
+        System.out.println("email:");
+        e = in.nextLine();
+        
+        
+        
+        
+        Persona p = new Persona (n,c,e);
+        
+        System.out.println("Dati in input:");
+        System.out.println(p.info());
+        
+        System.out.println("Dati di output:");
+        System.out.println("registrazione    : " + p.registrazioneAvvenuta());
+    }
+    
+}
