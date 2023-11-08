072be455c8e1ee0509298dfeffa1d46c228e33cc
claudio.ghetta <claudio.ghetta@AULAS51-PC22.itib.local>
2023-11-08 09:38:52 +0100

    esercitazione persona

--- src/teoria/Persona.java
+++ src/teoria/Persona.java
@@ -2,41 +2,77 @@
 package teoria;
 
 /**
- *
- * @author claudio.ghetta
+ * La classe Persona serve per sapere se una persona si è registrata
+ * @author Ghetta Claudio 3INA 2023
+ * @version 1.0
  */
 public class Persona {
     private String nome;
     private String cognome;
     private String email;
     
+    /**
+     * classe senza parametri
+     */
     public Persona () {
         
     }
-    
+    /**
+     * classe con parametri
+     * @param nome
+     * @param cognome
+     * @param email 
+     */
     public Persona (String nome ,String cognome, String email) {
        this.nome=nome;
        this.cognome=cognome;
        this.email=email;           
     }
-    
+    /**
+     * restituisce nome
+     * @return nome 
+     */
     public String getNome () {
         return nome;
     }
+    public void setNome (String nome) {
+      this.nome=nome;
+    }
+    
+    /**
+     * restituisce cognome
+     * @return cognome
+     */
     public String getCognome () {
         return cognome;
+    }
+    public void setCognome (String cognome) {
+      this.cognome=cognome;
+    /**
+     * restituisce email
+     * @return email 
+     */
     }
     public String getEmail () {
         return email;
     }
+    public void setEmail (String email) {
+      this.email=email;
+    }
     
+    /**
+     * 
+     * @return registrazione 
+     */
     public boolean registrazioneAvvenuta () {
         boolean registrazione;
-        if (email.equals(" ")) {
-        registrazione = false;
+        
+        if (email.equals("")) {
+            registrazione = false;
         } else {
-        registrazione = true;
+            registrazione = true;
         }
+        
         return registrazione;
     }
     
@@ -45,8 +81,7 @@ public class Persona {
         testo = "nome          : " + nome    + "\n" +
                 "cognome       : " + cognome + "\n" +
                 "email         : " + email   + "\n";
-        return testo;
-        
-    }
-    
+        return testo;        
+    }    
 }
+
