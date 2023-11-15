c8efb0f61b08e8a11d1c324ddf23530f35852daf
claudio.ghetta <claudio.ghetta@AULAS51-PC21.itib.local>
2023-11-15 10:17:35 +0100

    fatta classe numero1

---
+++ src/numero/Numero1Test.java
@@ -0,0 +1,28 @@
+
+package numero;
+
+import java.util.Scanner;
+
+public class Numero1Test {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        int n1;
+        
+        System.out.println("inserisci il valore del numero");
+        n1 = in.nextInt();
+        
+        Numero1 n = new Numero1 (n1);
+        
+        System.out.println("Dati in input:");
+        System.out.println(n.info());
+        
+        System.out.println("Dati in output:");
+        System.out.println(n.pariODispari());
+        System.out.println(n.maggioreMinoreUguale());
+        
+
+    }
+    
+}
