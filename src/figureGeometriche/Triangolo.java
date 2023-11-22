9d4924871ef6e08960fee217091cffeea8ba9844
claudio.ghetta <claudio.ghetta@AULAS51-PC22.itib.local>
2023-11-22 09:56:23 +0100

    classe Triangolo e TriangoloTest

---
+++ src/figureGeometriche/Triangolo.java
@@ -0,0 +1,120 @@
+//dati i lati di un triangolo stabilire il tipo perimetro e area
+package figureGeometriche;
+
+/**
+ *Classe per stabilire il tipo, perimetro e area di un triangolo
+ * @author claudio.ghetta
+ * @version 1.0
+ */
+public class Triangolo {
+    private float l1;
+    private float l2;
+    private float l3;
+    
+    /**
+     * classe senza parametri
+     */
+    public Triangolo () {
+        
+    }
+    /**
+     * classe con parametri
+     * @param l1
+     * @param l2
+     * @param l3 
+     */
+    public Triangolo(float l1, float l2, float l3) {
+        this.l1 = l1;
+        this.l2 = l2;
+        this.l3 = l3;
+    }
+    /**
+     * restituisce l1
+     * @return l1
+     */
+    public float getL1() {
+        return l1;
+    }
+
+    public void setL1(float l1) {
+        this.l1 = l1;
+    }
+
+    public float getL2() {
+        return l2;
+    }
+
+    public void setL2(float l2) {
+        this.l2 = l2;
+    }
+
+    public float getL3() {
+        return l3;
+    }
+
+    public void setL3(float l3) {
+        this.l3 = l3;
+    }
+    /**
+     * restituisce testo
+     * @return 
+     */
+    public String info (){
+        String testo;
+        testo = "l1: " + l1 +"\n" +
+                "l2: " + l2 +"\n" +
+                "l3: " + l3;
+        return testo;
+    }
+    /**
+     * restituisce il valore del perimetro
+     * @return 
+     */
+    public float perimetro () {
+        float p;
+        
+        p = l1+l2+l3;
+        
+        return p;    
+    }
+    
+    /**
+    * Restituisce il valore del semi-perimetro
+    * @return 
+    */
+    private float calcolaSemiperimetro() {
+      float semiperimetro;
+      
+      semiperimetro = perimetro()/2;
+      
+      return semiperimetro; 
+    }
+    
+    /**
+    * Restituisce il valore dell area
+    * @return 
+    */
+    public float area() {
+      float area;
+      
+      area = (float) Math.sqrt(calcolaSemiperimetro()*(calcolaSemiperimetro()-l1)
+              *(calcolaSemiperimetro()-l2*(calcolaSemiperimetro()-l3)));
+    
+      return area;
+    }
+    
+    public String tipo() {
+        String tipo ="";
+        
+        if ((l1 == l2 && l2==l3))
+            tipo = "equilatero";
+        if (l1 != l2 && l1 != l3 && l2 != l3)
+            tipo = "scaleno";
+        if ((l1 == l2 && l2 != l3) || (l2 == l3 && l2 != l1) || (l1 == l3 && l1 != l2))
+            tipo = "isoscele";
+        
+        return tipo;
+    }
+    
+    
+}
---
+++ src/figureGeometriche/TriangoloTest.java
@@ -0,0 +1,36 @@
+
+package figureGeometriche;
+
+import java.util.Scanner;
+
+public class TriangoloTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        
+        float l1, l2, l3;   
+        
+        System.out.println("inserisci il primo lato:");
+        l1=in.nextFloat();
+        
+        System.out.println("inserisci il secondo lato:");
+        l2=in.nextFloat();        
+        
+        System.out.println("inserisci il terzo lato");
+        l3=in.nextFloat();
+                
+        Triangolo t = new Triangolo (l1, l2, l3);
+        
+        System.out.println("dati in input:" + t.info());
+        System.out.println(t.perimetro());
+        System.out.println(t.area());
+        System.out.println(t.tipo());
+
+    }
+    
+}
+
+
+
+
+
