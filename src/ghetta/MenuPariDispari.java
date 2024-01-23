bf9754a40536c7a4cf4102cfe2f21b0f9aacae4f
claudio <claudio@minion7.homenet.telecomitalia.it>
2024-01-23 19:04:39 +0100

    create classi MenuPariDispari e MenuPariDispariTest

--- src/ghetta/MenuPariDispari.java
+++ src/ghetta/MenuPariDispari.java
@@ -1,26 +1,46 @@
 
 package ghetta;
 
-
+/**
+ * Scrivere una classe e il relativo test con menù che, dato un numero n, 
+ * preveda due metodi: crea una stringa con i primi n numeri dispari (for),
+ * crea una stringa con i primi n numeri pari (while)
+ * @author claudio.ghetta
+ * @version 1.0
+ */
 public class MenuPariDispari {
     private int numero;
-    
+    /**
+     * costruttore senza parametri
+     */
     public MenuPariDispari() {
         
     }
-    
+    /**
+     * costruttore con parametri
+     * @param numero 
+     */
     public MenuPariDispari(int numero) {
         this.numero = numero;
     }
-
+    /**
+     * restituisce numero
+     * @return 
+     */
     public int getNumero() {
         return numero;
     }
-
+    /**
+     * imposta/modifica numero
+     * @param numero 
+     */
     public void setNumero(int numero) {
         this.numero = numero;
     }
-    
+    /**
+     * restituisce info
+     * @return 
+     */
     public String info(){
         String testo;
         
@@ -29,7 +49,10 @@ public class MenuPariDispari {
         return testo;
         
     }
-    
+    /**
+     * restituisce una stringa di valore
+     * @return 
+     */
     public int dispari(){
         int valore=0;
         
@@ -46,7 +69,10 @@ public class MenuPariDispari {
         return valore;
         
     }
-    
+    /**
+     * restituisce una stringa di valore
+     * @return 
+     */
     public int pari(){
         int valore=0;
         int contatore =0;
