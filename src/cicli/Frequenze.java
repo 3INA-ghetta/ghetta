ccfb5beb8807a24a369d5bb44f7c212692d83920
claudio <claudio@minion7.homenet.telecomitalia.it>
2024-01-30 21:58:01 +0100

    create classi Frequenze e FrequenzeTest

--- src/cicli/Frequenze.java
+++ src/cicli/Frequenze.java
@@ -6,47 +6,47 @@ package cicli;
  * @author claudio.ghetta
  */
 public class Frequenze {
-    double frequenza = Math.random();
-    double frequenza1 = Math.random();
-    double frequenza2 = Math.random();
-    double frequenza3 = Math.random();
-    int intero;
-    int intero1;
-    int intero2;
-    int intero3;
-   
-   public String frequenzaAssoluta(){
+    
+    private int cont = 0;
 
-       String testo;
+    public Frequenze() {
+    }
+
+    public int getCont() {
+        return cont;
+    }
+
+    public void setCont(int cont) {
+        this.cont = cont;
+    }
+    
+    public String frequenza(){
+       String testo = "";
+       double frequenza;
+       double frePer;
+       double freRe;
+       double oggetto;
        
-       intero = (int)frequenza;
-       intero1 = (int)frequenza1;
-       intero2 = (int)frequenza2;
-       intero3 = (int)frequenza3;
+       while (cont <= 4){
+       frequenza = Math.random();
+       oggetto = Math.random();
        
-       testo = "frequenza 1:" + intero +"\n"
-               + "frequenza 2:" + intero1 +"\n"
-               + "frequenza 3:" + intero2 +"\n"
-               + "frequenza 4:" + intero3;
+       System.out.println ( "frequenza assoluta n " + cont + " é: " + frequenza);
+       
+       freRe = frequenza + oggetto;
+       
+       System.out.println("frequenza relativa n " + cont + ": " + "\n"+ "é: " +freRe );
+       
+       frePer = freRe * 100;
+       
+       System.out.println("frequenza percentuale n " + cont + ": " + "\n" + " é: " +frePer + "%" );
+       
+       cont++;
+       
+       }
        
        return testo;
-   }
-   
-   public String frequenzaRelativa(){
-
-       String testo;
-       
-       intero = (int)frequenza;
-       intero1 = (int)frequenza1;
-       intero2 = (int)frequenza2;
-       intero3 = (int)frequenza3;
-       
-       testo = "frequenza 1:" + intero +"\n"
-               + "frequenza 2:" + intero1 +"\n"
-               + "frequenza 3:" + intero2 +"\n"
-               + "frequenza 4:" + intero3;
-       
-       return testo;
-   }
+    }
        
 }
+
--- src/cicli/FrequenzeTest.java
+++ src/cicli/FrequenzeTest.java
@@ -3,5 +3,14 @@ package cicli;
 
 
 public class FrequenzeTest {
+
+    public static void main(String[] args) {
+        
+        
+        Frequenze f = new Frequenze();
+        
+        System.out.println(f.frequenza());
+
+    }
     
 }
