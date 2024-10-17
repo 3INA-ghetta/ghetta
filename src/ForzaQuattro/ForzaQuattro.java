b1334459daa5bd3aaaab477244b3c4bc597e3733
claudio <claudio@minion7.homenet.telecomitalia.it>
2024-10-17 21:52:30 +0200

    create classi CifrarioDiCesare

--- src/ForzaQuattro/ForzaQuattro.java
+++ src/ForzaQuattro/ForzaQuattro.java
@@ -1,198 +1,120 @@
 
 package ForzaQuattro;
 
-import java.util.Objects;
-
+import java.util.Scanner;
 
 public class ForzaQuattro {
+    private static final int RIGHE = 6;
+    private static final int COLONNE = 7;
+    private static char[][] campo = new char[RIGHE][COLONNE];
+    private static char giocatore;
     
-    private static final int Righe = 6;
-    private static final int Colonne = 7;
-    private String [][] campo = new String [Righe][Colonne];
+    
 
-    public ForzaQuattro() {
+    public static void main(String[] args) {
         riempiCampo();
-    }
+        giocatore = 'X';
+        boolean a = true;
 
-    public String[][] getCampo() {
-        return campo;
-    }
-
-    public void setCampo(String[][] campo) {
-        this.campo = campo;
-    }
- 
-    public void riempiCampo() {
-        for (int i = 0; i < campo.length; i++) {
-            for (int j = 0; j < campo.length; j++) {
-                if (campo[i][j] != "X" && campo[i][j] != "O") {
-                    campo[i][j] = "_";
-                }
+        while (a == true) {
+            stampaCampo();
+            int colonna = mossa();
+            int riga = mettiDisco(colonna);
+            if (riga == -1) {
+                System.out.println("colonna piena.");
             }
 
-        }
-    }
-    
-    public String vCampo() {
-        String testo = "";
-        for (int i = 0; i < campo.length; i++) {
-            for (int j = 0; j < campo.length; j++) {
-                testo += campo[i][j] + "_";
+            if (vincita(riga, colonna)) {
+                stampaCampo();
+                System.out.println("Giocatore " + giocatore + " vince!");
+                a = false;
             }
-            testo += "\n";
-        }
-        return testo;
-    }
-    
-    
-    private String giocatore(){
-        int i =2;
-        if (i % 2 == 0){
-            i--;
-            return "X";
-        }else{
-            i++;
-            return "O";
-        }
-    }
-    
-    private boolean isCellaDisponibile(int riga, int colonna) {
-        if (Objects.equals(campo[riga][colonna], "_")) {
-            return true;
-        } else {
-            while (Objects.equals(campo[riga][colonna], "_") && riga < 5){
-                riga++;
-            }
-            if (riga >= 5){
-                return false;
-            }else{
-                return true;
-            }
-            
-        }
-    }
-    
-    public String mettiPezzo(int riga, int colonna){ //fare sottomatrice per diagonale
-        
-        int i = 0;
-        String next = vincita();
-        
-        while (next.equals("X")){
-            if ( i < campo.length && i >= 0 && colonna < campo.length && colonna >= 0 && isCellaDisponibile(riga,colonna)){
-                campo[i][colonna]= giocatore();
-            }
-        }
-        
-        return vincita();
- 
-    }
-    
-    private String vincita (){
-        
-        int contX = 0;
-        int contO = 0;
+
+            if (giocatore == 'X'){
                 
-        for (int i = 0; i < campo.length; i++) {
-            for (int j = 0; j < campo.length; j++) {
-                if (Objects.equals(campo[i][j], "X")) {
-                    contX++;
-                    if (contX == 4) {
-                        return "X";
-                    }
-                }
-                
-                if (Objects.equals(campo[i][j], "O")) {
-                    contO++;
-                    if (contO == 4) {
-                        return "O";
-                    }
-                }
-            }
+                giocatore = 'O';
             
-            contX = 0;
-            contO = 0;
+            }else {
+                giocatore = 'X';    
+                    }
         }
 
-        // Colonne    
-        for (int i = 0; i < campo.length; i++) {
-            for (int j = 0; j < campo.length; j++) {
-                if (Objects.equals(campo[j][i], "X")) {
-                    contX++;
-                    if (contX == 4) {
-                        return "X";
-                    }
-                }
-                
-                if (Objects.equals(campo[j][i], "O")) {
-                    contO++;
-                    if (contO == 4) {
-                        return "O";
-                    }
-                }
-            }
-            
-            contX = 0;
-            contO = 0;
-        }
-
-        // Diagonale principale
-        
-        for (int i = 0; i < campo.length; i++) {
-            if (Objects.equals(campo[i][i], "X")) {
-                contX++;
-                if (contX == 4) {
-                    return "X";
-                }
-            }
-            
-            if (Objects.equals(campo[i][i], "O")) {
-                contO++;
-                if (contO == 4) {
-                    return "O";
-                }
-            }
-        }                
-
-        // Diagonale secondaria
-        int riga = campo.length-1;
-        for (int i = riga; i >= 0; i--) {
-            int offset = riga-1;
-            
-            if (Objects.equals(campo[i][offset], "X")) {
-                contX++;
-                if (contX == 4) {
-                    return "X";
-                }
-            }
-            
-            if (Objects.equals(campo[i][offset], "O")) {
-                contO++;
-                if (contO == 4) {
-                    return "O";
-                }
-            }
-        }            
-        
-        // Parità
-        int cont = 0;
-        for (int i = 0; i < campo.length; i++) {
-            for (int j = 0; j < campo.length; j++) {
-                if (Objects.equals(campo[i][j], "X") || 
-                                      Objects.equals(campo[i][j], "O")) {
-                    cont++;
-                    
-                    if (cont == 6 * 7) {
-                        return "=";
-                    }
-                }
-            }
-        }
-        
-        // Nessuna vincita
-        return "";
-        
+        System.out.println("fine");
     }
-    
-    
-    
+
+    private static void riempiCampo() {
+        for (int r = 0; r < RIGHE; r++) {
+            for (int c = 0; c < COLONNE; c++) {
+                campo[r][c] = '.';
+            }
+        }
+    }
+
+    private static void stampaCampo() {
+        for (int r = 0; r < RIGHE; r++) {
+            for (int c = 0; c < COLONNE; c++) {
+                System.out.print(campo[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int mossa() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Giocatore " + giocatore + ", scegli una colonna da 0 a 6: ");
+        return in.nextInt();
+    }
+
+    private static int mettiDisco(int colonna) {
+        if (colonna < 0 || colonna >= COLONNE) {
+            System.out.println("colonna sbagliata riprova");
+            return -1;
+        }
+
+        for (int r = RIGHE - 1; r >= 0; r--) {
+            if (campo[r][colonna] == '_') {
+                campo[r][colonna] = giocatore;
+                return r; 
+            }
+        }
+        return -1; 
+    }
+
+    private static boolean vincita(int riga, int colonna) {
+        return direzione(riga, colonna, 1, 0) || 
+               direzione(riga, colonna, 0, 1) || 
+               direzione(riga, colonna, 1, 1) || 
+               direzione(riga, colonna, 1, -1);  
+    }
+
+    private static boolean direzione(int riga, int colonna, int rigaSecondaria, int colonnaSecondaria) {
+        char pedina = campo[riga][colonna];
+        int conta = 0;
+
+
+        for (int d = 0; d < 4; d++) {
+            int r = riga + d * rigaSecondaria;
+            int c = colonna + d * colonnaSecondaria;
+            if (r >= 0 && r < RIGHE && c >= 0 && c < COLONNE && campo[r][c] == pedina) {
+                conta++;
+            } else {
+                break;
+            }
+        }
+
+        for (int d = 1; d < 4; d++) {
+            int r = riga - d * rigaSecondaria;
+            int c = colonna - d * colonnaSecondaria;
+            if (r >= 0 && r < RIGHE && c >= 0 && c < COLONNE && campo[r][c] == pedina) {
+                conta++;
+            } else {
+                break;
+            }
+        }
+        
+        return conta >= 4;
+    }
 }
+
+    
+
